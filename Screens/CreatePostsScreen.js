e03a6d5cfa13c1637a80386709b70310999ed6fe
Olevova <olevova1983@gmail.com>
2023-04-05 22:28:36 +0300

    photo

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -2,36 +2,90 @@ import {
     StyleSheet,
     View,
     Text,
-    Touchable,
     Image,
+    TouchableOpacity
 
 } from "react-native";
-import React, { useState } from "react";
-import { Camera } from "expo-camera";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 
-export const CreatePostsScreen = () => {
-    const [camera, setCamera] = useState(null);
-    const [photo, setPhoto] = useState(null)
+export const CreatePostsScreen = ({navigation}) => {
+    const [photo, setPhoto] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(CameraType.back);
     
+    useEffect(() => {
+        (async () => {
+            console.log(type, 1, photo);
+            setPhoto(null);
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        console.log(status);
+        // await MediaLibrary.requestPermissionsAsync();
+
+        setHasPermission(status === "granted");
+    })();
+    }, []);
+
+    console.log(navigation);
+
+    if (hasPermission === null) {
+            return <View />;
+        }
+    if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+        }
+    
+    function toggleCameraType() {
+        console.log("click");
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    };
+
     const takePhoto = async () => {
-        const photoCamera = await camera.takePictureAsync();
-        setPhoto(photoCamera.uri);
-        console.log(photoCamera);
+        if (cameraRef) {
+            // console.log(cameraRef.takePictureAsync());
+            const data = await cameraRef.takePictureAsync();
+            console.log(data.uri);
+            setPhoto(data.uri)
+        }
+            // await MediaLibrary.createAssetAsync(uri);
     }
 
+    const sendPhoto = async () => {
+        await navigation.navigate("Posts", { photo })
+        setPhoto(null);
+    }
     return (
         <View style={styles.home}>
-            <Camera style={styles.camerastyle} ref={setCamera}>
-                {photo && (<View style={styles.takePhotoContainer}>
-                             <Image source={{ uri: photo }} style={{ height: 200, width: 200 }} />
-                        </View>)}
-                <TouchableOpacity onPress={takePhoto} style={styles.containercamera}>
+            <Camera style={styles.camerastyle}
+                type={type}
+                ref={ref => setCameraRef(ref)}
+            >
+                <TouchableOpacity
+                    style={styles.flipContainer}
+                    onPress={toggleCameraType}
+                >
+                <Text style={{ fontSize: 18, marginBottom: 10, color: "#ffff" }}>
+                    {" "}
+                    Flip Camera{" "}
+                </Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.containercamera} onPress={()=>takePhoto()}>
                     <Text style={styles.textsnap}>SNAP</Text>
                 </TouchableOpacity>
+                {photo && (<View style={styles.takePhotoContainer}>
+                                <Image source={{ uri : photo }} style={{ height: 200, width: 200 }} />
+                            </View>)}
             </Camera>
-            {/* <Text style={styles.textHome}>CreatePostsScreen</Text> */}
+            <View style={styles.load}>
+                 <TouchableOpacity style={styles.containerLoad} onPress={()=>sendPhoto()}>
+                    <Text style={styles.textsnap}>LOAD</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 };
@@ -45,6 +99,10 @@ const styles = StyleSheet.create(
             borderTopLeftRadius: 20 ,
             borderTopRightRadius: 20 ,
         },
+        flipContainer: {
+            flex: 0.2,
+            alignSelf: "flex-end",
+         },
         textsnap: {
             // margin: "auto",
             
@@ -52,22 +110,24 @@ const styles = StyleSheet.create(
             // textAlign: 'center',
         },
         camerastyle:{
-            // height: 300,
-            flex:1,
-            marginTop: 30, 
+            height: '80%',
+            marginTop: 20, 
             justifyContent: 'flex-end',
             alignItems: 'center',
+            borderRadius: 50,
+            borderWidth:1,
+            marginHorizontal:2,
             
         },
         containercamera:{
             borderWidth: 1,
-    borderColor: "#ffff00",
-    width: 70,
-    height: 70,
-    borderRadius: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
+            borderColor: "#ffff00",
+            width: 70,
+            height: 70,
+            borderRadius: 50,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 20,
 
         },
         takePhotoContainer: {
@@ -78,6 +138,20 @@ const styles = StyleSheet.create(
             borderWidth: 1,
             // height: 200,
             // width:200
+        },
+        containerLoad:{
+            borderWidth: 1,
+            borderColor: "#ffff00",
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: 'center',
+            height:40,
+            marginTop: 20,
+            width:200
+            
+        },
+        load: {
+            alignItems: "center"
         }
 
     }
--- Screens/PostsScreen.js
+++ Screens/PostsScreen.js
@@ -1,14 +1,35 @@
+import React,{ useEffect, useState } from "react";
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Image,
+    FlatList
 } from "react-native";
 
 
-export const PostsScreen = () => {
+export const PostsScreen = ({ route }) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        if (route.params) {
+            setPosts((prevState) => [...prevState, route.params]);
+        }
+    }, [route.params])
+    console.log(posts);
+    
     return (
         <View style={styles.home}>
-            <Text style={styles.textHome}>PostScreen</Text>
+            <FlatList 
+                data={posts}
+                keyExtractor={(i, indx) => { console.log(indx.toString());return indx.toString() }}
+                renderItem={({item}) =>(<View style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+                        <Image source={{ uri: item.photo }} style={{width:200, height:200}} />
+                    </View>)}
+            />
         </View>
     )
 };
